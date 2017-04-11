dae7c93ceda4c74e266a0d0c0b62a6a403970f9c
jadesungod <jadesungod@gmail.com>
2017-04-10 21:28:10 -0400

    Create makeCacheMatrix.R

---
+++ makeCacheMatrix.R
@@ -0,0 +1,26 @@
+makeCacheMatrix <- function(m = matrix()) {
+	ivs <- NULL
+	set <- funtion(n) {
+		m <<- n
+		ivs <<- NULL
+	}
+	
+	get <- function(m) x
+	setivs <- function(inverse) ivs <<- inverse
+	getivs <- function() ivs
+	
+	list(set = set, get = get, setinverse = setivs, getinverse = getivs)
+}
+
+cachesolve <- function(x, ...) {
+	ivs <- x$getivs()
+	
+	if (!is.null(inv)) {
+		message("cached matrix exist!)
+		return(ivs)
+	}
+	
+	matrix1 <- x$get()
+	ivs <- solve(matrix1)
+	ivs
+}
