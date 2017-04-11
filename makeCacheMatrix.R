ed0035b0597bcf22151215cbdc71b0912218bc5a
Gary Sun <jadesungod@gmail.com>
2017-04-11 07:10:03 -0400

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,25 +1,26 @@
-makeCacheMatrix <- function(m = matrix()) {
-	ivs <- NULL
-	set <- funtion(n) {
+##cache the matrix
+makeCacheMatrix <- function(m = matrix()) {    
+	ivs <- NULL                    
+	set <- funtion(n) {                             ##set the value of the matrix
 		m <<- n
 		ivs <<- NULL
 	}
 	
-	get <- function(m) x
-	setivs <- function(inverse) ivs <<- inverse
-	getivs <- function() ivs
+	get <- function(m) x                            ##get the value of the matrix
+	setivs <- function(inverse) ivs <<- inverse     ##set the inverse of the matrix
+	getivs <- function() ivs                        ##get the inverse of the matrix     
 	
 	list(set = set, get = get, setinverse = setivs, getinverse = getivs)
 }
 
 cachesolve <- function(x, ...) {
 	ivs <- x$getivs()
-	
-	if (!is.null(inv)) {
+	##check if the matrix is cached already
+	if (!is.null(inv)) {             
 		message("cached matrix exist!")
-		return(ivs)
+		return(ivs)                             ##skip the computation
 	}
-	
+	##compute the inverse of the matrix
 	matrix1 <- x$get()
 	ivs <- solve(matrix1)
 	ivs
